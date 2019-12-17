672ed2dcf6468e3d122f41d337f46fac31b6aa57
Balazs Koskoci <Balazs.Koskoci@logmein.com>
2019-12-17 14:11:52 +0100

    Add autocomplete on dish input field

--- lib/shopping_list_web/controllers/item_controller.ex
+++ lib/shopping_list_web/controllers/item_controller.ex
@@ -11,22 +11,19 @@ defmodule ShoppingListWeb.ItemController do
 
   def new(conn, _params) do
     changeset = Recipes.change_item(%Item{})
-    ingredients = Recipes.list_ingredients()
 
-    render(conn, "new.html", changeset: changeset, ingredients: ingredients)
+    render_new(conn, changeset)
   end
 
   def create(conn, %{"item" => item_params}) do
     case Recipes.create_item(item_params) do
-      {:ok, item} ->
+      {:ok, _item} ->
         conn
         |> put_flash(:info, "Item created successfully.")
         |> redirect(to: Routes.item_path(conn, :index))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        ingredients = Recipes.list_ingredients()
-
-        render(conn, "new.html", changeset: changeset, ingredients: ingredients)
+        render_new(conn, changeset)
     end
   end
 
@@ -37,5 +34,12 @@ defmodule ShoppingListWeb.ItemController do
     conn
     |> put_flash(:info, "Item deleted successfully.")
     |> redirect(to: Routes.item_path(conn, :index))
+  end
+
+  defp render_new(conn, changeset) do
+    ingredients = Recipes.list_ingredients()
+    dishes = Recipes.list_dishes() |> Enum.join(", ")
+
+    render(conn, "new.html", changeset: changeset, ingredients: ingredients, dishes: dishes)
   end
 end
